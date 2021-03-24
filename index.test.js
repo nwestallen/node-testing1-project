33d0dda6fedd6e62887e73c1807f064aae250d2b
Nick <nwestallen@gmail.com>
2021-03-24 11:50:58 -0400

    trimPropertiesMutation tests

--- index.test.js
+++ index.test.js
@@ -20,9 +20,16 @@ describe('[Exercise 1] trimProperties', () => {
 describe('[Exercise 2] trimPropertiesMutation', () => {
   it('[3] returns an object with the properties trimmed', () => {
     // ✨ test away
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
   })
   it('[4] the object returned is the exact same one we passed in', () => {
     // ✨ test away
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(input)
   })
 })
 
