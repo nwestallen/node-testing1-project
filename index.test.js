afd1d87662255957e4485bed5e3ab9ba9d427fe8
Nick <nwestallen@gmail.com>
2021-03-24 11:53:34 -0400

    findLargestInteger test

--- index.test.js
+++ index.test.js
@@ -36,6 +36,9 @@ describe('[Exercise 2] trimPropertiesMutation', () => {
 describe('[Exercise 3] findLargestInteger', () => {
   it('[5] returns the largest number in an array of numbers', () => {
     // ✨ test away
+    const input = [-2, 4, 8, 1, 0]
+    const actual = utils.findLargestInteger(input)
+    expect(actual).toEqual(8)
   })
 })
 
